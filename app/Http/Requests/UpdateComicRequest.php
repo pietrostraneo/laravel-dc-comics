71960dfea6df5ba4a355f3ab20861c1db642b9bf
pietrostraneo <straneo_p@yahoo.it>
2024-02-20 18:03:05 +0100

    Translated errors from italian to english

--- app/Http/Requests/UpdateComicRequest.php
+++ app/Http/Requests/UpdateComicRequest.php
@@ -27,8 +27,8 @@ class UpdateComicRequest extends FormRequest
 
             'title' => 'required|max:50|min:5',
             'description' => 'required|max:700|min:5',
-            'thumb' => 'required|min:20',
-            'price' => 'required|max:20',
+            'thumb' => 'required|min:15',
+            'price' => 'required|max:15',
             'series' => 'required|max:50',
             'sale_date' => 'required|date',
             'type' => 'required|max:30',
@@ -42,24 +42,24 @@ class UpdateComicRequest extends FormRequest
     {
         return [
 
-            'title.required' => 'Il titolo è obbligatorio!',
-            'title.max' => 'Il titolo deve essere al massimo di 50 caratteri!',
-            'title.min' => 'Il titolo deve essere lungo almeno 5 caratteri!',
-            'description.required' => 'La descrizione è obbligatoria!',
-            'description.max' => 'La descrizione deve essere al massimo di 400 caratteri!',
-            'description.min' => 'La descrizione deve essere lunga almeno 5 caratteri!',
-            'thumb.required' => 'L\'immagine è obbligatoria!',
-            'thumb.min' => 'Il link dell\'immagine deve essere lunga almeno 20 caratteri!',
-            'price.required' => 'Il prezzo è obbligatorio!',
-            'price.max' => 'Il prezzo deve essere al massimo di 20 caratteri!',
-            'series.required' => 'Il nome della serie è obbligatorio!',
-            'series.max' => 'Il nome della serie deve essere al massimo di 50 caratteri!',
-            'sale_date.required' => 'La data è obbligatoria!',
-            'sale_date.date' => 'La data deve essere nel formato YYYY-MM-DD!',
-            'type.required' => 'La tipologia del fumetto è obbligatoria!',
-            'type.max' => 'La tipologia del fumetto deve essere al massimo di 30 caratteri!',
-            'artists.required' => 'I nomi degli artisti sono obbligatori!',
-            'writers.required' => 'I nomi degli scrittori sono obbligatori!'
+            'title.required' => 'The title of the comic is mandatory',
+            'title.max' => 'The title can be a max of 50 characters length',
+            'title.min' => 'The title must be at least 5 characters length',
+            'description.required' => 'Description is mandatory',
+            'description.max' => 'The description can be a max of 700 characters length',
+            'description.min' => 'The description must be at least 5 characters length',
+            'thumb.required' => 'Image is mandatory',
+            'thumb.min' => 'Image URL must be at least 15 characters length',
+            'price.required' => 'Price is mandatory',
+            'price.max' => 'Price can be a max of 10 characters length',
+            'series.required' => 'Series name is mandatory',
+            'series.max' => 'Series name can be a max of 50 characters length',
+            'sale_date.required' => 'Sale Date is mandatory',
+            'sale_date.date' => 'The date must be in the format YYYY-MM-DD',
+            'type.required' => 'Comic/s type is mandatory',
+            'type.max' => 'Comic/s type can be a max of 30 characters length',
+            'artists.required' => 'Artists name are mandatory',
+            'writers.required' => 'Writers name are mandatory'
 
         ];
     }
